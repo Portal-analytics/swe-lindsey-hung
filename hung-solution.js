79c15b7e17720c9ad4cd4b2cf97afdc0130ce97a
Hct13345 <htang97@gmail.com>
2017-05-24 09:13:39 -0400

    Add files via upload

---
+++ hung-solution.js
@@ -0,0 +1,26 @@
+
+const numbers = [];
+var len = 0
+var sum = 0
+
+for (var i = 0; i < 1001; i++) {
+        if (i%5 == 0);
+            numbers.append(i)
+            len += 1
+    } 
+
+for (var j = 0; j < 1001; j++) {
+        if (j%3 == 0) & j not in numbers;
+            numbers.append(j)
+            len += 1
+    }
+
+for (var x = 0; x < len; x++) {
+    {numbers(i)} += sum
+}
+
+
+ReactDOM.render(
+    sum,
+    document.getElementById('root)
+);
