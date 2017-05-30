28810a01fbea1ed3fb4029a63b4ceb106a93cc22
Lindsey Abramson <abramsl5@tcnj.edu>
2017-05-30 16:03:48 -0400

    Got all of Challenge 5 working

--- Challenge5/challenge5/src/Form.js
+++ Challenge5/challenge5/src/Form.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 
+var countReqs = 0;
 class Form extends Component {
     constructor(props) {
         super(props)
@@ -19,30 +20,93 @@ class Form extends Component {
         console.log(this.state);
     }
 
+    handleEditChange(field, event) {
+        this.setState({
+            ...this.state,
+            [field]: event.target.value
+        });
+        console.log(this.state);
+    }
+
     handleClick(event) {
+        /*Challenge 4
         var entry=[];
         entry.push(this.state.contract);
         entry.push(this.state.description);
         entry.push(this.state.price);
-        let newContracts = this.state.reqs.concat([entry]);
+
+        let newContracts = this.state.reqs.concat([entryObj]);
         this.setState({
             contract:this.state.contract,
             description: this.state.description,
             price: this.state.price,
             reqs: newContracts
         })
+        */
+        var entryArr = this.state.reqs,
+        updatedReqs = this.state.reqs.concat(
+        [
+            {contract: this.state.contract,
+            description: this.state.description,
+            price: this.state.price,
+            index: countReqs}
+        ]);
+        
+        countReqs++;
+
+        this.setState({
+            ...this.state,
+            reqs: updatedReqs
+        })
+
       console.log(this.state)
       event.preventDefault();
     }
 
+    handleEdit(req) {
+        const updatedReqs = this.state.reqs.slice(0);
+        updatedReqs[req.index].editing = true;
+
+        this.setState({
+            ...this.state,
+            reqs: updatedReqs
+        });
+        
+        console.log(this.state);
+    }
+
+    handleEditDone(req) {
+        const updatedReqs = this.state.reqs.slice(0);
+
+        updatedReqs[req.index].editing = false;
+        updatedReqs[req.index].contract = this.state.contract;
+        updatedReqs[req.index].description = this.state.description;
+        updatedReqs[req.index].price = this.state.price;
+
+        this.setState({
+            ...this.state,
+            reqs: updatedReqs
+        });
+        console.log(this.state);
+    }
+
     render() {
-        const contractList = this.state.reqs.map(
-            (c) => {
-                return(
-                    <li> {"Contract: " + c[0] + " Description: " + c[1] + " Price: " + c[2]} </li> 
-                );
-            }
-        );
+        const contractList = this.state.reqs.map(req => {
+        return (
+            <li> 
+                Name: {!req.editing && req.contract + " "} 
+                {req.editing &&  <input type="text" value={this.state.contract} onChange={(event) => this.handleChange('contract', event)} />}
+     
+                Description: {!req.editing && req.description + " "} 
+                {req.editing &&  <input type="text" value={this.state.description} onChange={(event) => this.handleChange('description', event)} />}
+      
+                Price: {!req.editing && req.price + " "}
+                {req.editing &&  <input type="text" value={this.state.price} onChange={(event) => this.handleChange('price', event)} />}
+
+                <button onClick = {() => {this.handleEdit(req)}}> Edit </button > 
+                <button onClick = {() => {this.handleEditDone(req)}}> Done </button>
+            </li>)
+})
 
         console.log(contractList);
 
