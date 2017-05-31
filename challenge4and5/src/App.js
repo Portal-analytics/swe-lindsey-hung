fac4c2c75fce0aaf9213c022c098a20dc3581e0f
Hung Tang <HungTang@Hungs-MBP.lan>
2017-05-31 10:02:19 -0400

    what we have

--- challenge4and5/src/App.js
+++ challenge4and5/src/App.js
@@ -3,19 +3,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js'
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+import * as firebase from 'firebase'
 
 class App extends Component {
+
+constructor() {
+  super();
+  this.state = {
+    speed: 25
+  };
+}
+
+componentDidMount() {
+  const rootRef = firebase.database().ref().child('react');
+  const speedRef = rootRef.child('speed');
+  speedRef.on('value',snap => {
+    this.setState({
+      speed: snap.val()
+    })
+  });
+}
+
   render() {
     return (
       <div className="App">
         <MuiThemeProvider>
           <AppBar title='Portal Analytics' showMenuIconButton={false}/>
         </MuiThemeProvider>
-
         <Form />
+        <h1>{this.state.speed}</h1>
       </div>
     );
   }
--- challenge4and5/src/index.js
+++ challenge4and5/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyCDZi-fb6kX-4e6S-EwNloOuV7D_v5PoP8",
+    authDomain: "contract-starter.firebaseapp.com",
+    databaseURL: "https://contract-starter.firebaseio.com",
+    projectId: "contract-starter",
+    storageBucket: "contract-starter.appspot.com",
+    messagingSenderId: "99212900703"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
